94d3339e2116ce14f0f7908689b524cc0c9fe270
Antônio Evaldo <antonio.evaldo349@gmail.com>
2022-09-08 09:47:00 -0300

    aula 04 e 05

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,6 +4,10 @@ const PessoaController = require('../controllers/PessoaController');
 const router = Router();
 
 router
-    .get('/pessoas', PessoaController.obtemTodasAsPessoas);
+    .get('/pessoas', PessoaController.obtemTodasAsPessoas)
+    .get('/pessoas/:id', PessoaController.obtemUmaPessoa)
+    .post('/pessoas', PessoaController.criaPessoa)
+    .put('/pessoas/:id', PessoaController.atualizaPessoa)
+    .delete('/pessoas/:id', PessoaController.apagaPessoa)
 
 module.exports = router;
