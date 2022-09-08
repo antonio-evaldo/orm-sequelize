56f79fdfb8ded88a7ae758655a34bc2ea1f51e80
Antônio Evaldo <antonio.evaldo349@gmail.com>
2022-09-08 12:49:42 -0300

    aula 06

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -9,5 +9,9 @@ router
     .post('/pessoas', PessoaController.criaPessoa)
     .put('/pessoas/:id', PessoaController.atualizaPessoa)
     .delete('/pessoas/:id', PessoaController.apagaPessoa)
+    .get('/pessoas/:estudanteId/matriculas/:matriculaId', PessoaController.obtemUmaMatricula)
+    .post('/pessoas/:estudanteId/matriculas', PessoaController.criaMatricula)
+    .put('/pessoas/:estudanteId/matriculas/:matriculaId', PessoaController.atualizaMatricula)
+    .delete('/pessoas/:estudanteId/matriculas/:matriculaId', PessoaController.apagaMatricula)
 
 module.exports = router;
